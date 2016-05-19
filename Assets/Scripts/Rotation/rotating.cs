b6ea5842b3bc77e6b78b494bc2e6641909079130
xraymonx <raymon_jesse2@hotmail.com>
2016-05-19 20:32:38 +0200

    Rotation to the cursor + custom cursor.

---
+++ Assets/Scripts/Rotation/rotating.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotating : MonoBehaviour {
+
+	private bool _shouldIRotate = false;
+
+
+	void OnMouseDown()
+	{
+		_shouldIRotate = true;
+	}
+
+	void Update()
+	{
+		Debug.Log (_shouldIRotate);
+		if (_shouldIRotate) {
+			Debug.Log ("Keep doing it");
+			Rotate ();
+		}
+	}
+
+	void Rotate()
+	{
+		Vector3 mousePos = Input.mousePosition;
+
+		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+
+		float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+
+		if (Input.GetMouseButtonUp(0)) 
+		{
+			_shouldIRotate = false;
+		}
+	}
+}
+

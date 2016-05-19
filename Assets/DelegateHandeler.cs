ef5d3712568a462ab87ef3f7dcac7624d38d8b5c
DavidZwit <davidzwitser@gmail.com>
2016-05-19 21:08:37 +0200

    Adding light source size handeler

---
+++ Assets/DelegateHandeler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateHandeler : MonoBehaviour {
+
+    public delegate void LazerHit(GameObject hit);
+    public static event LazerHit OnLazerHit;
+
+    public delegate void LazerLeave(GameObject hit);
+    public static event LazerHit OnlazerLeave;
+
+    public static void LazerHitEvent(GameObject hit)
+    {
+        OnLazerHit(hit);
+    }
+
+    public static void LazerLeaveEvent(GameObject hit)
+    {
+        OnlazerLeave(hit);
+    }
+}

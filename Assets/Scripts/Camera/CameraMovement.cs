7807957431c0a30427303e8890b6786321fd52dd
nathan-san <18964@ma-web.nl>
2016-05-20 06:41:57 +0200

    Camera now has boundaries and smooth movement.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-	[SerializeField] private float _speed = 3f;
+	[SerializeField] private float _speed = 10f;
 	[SerializeField] private float _boundary = 30f;
 
 	private int _theScreenWidth;
 	private int _theScreenHeight;
 
-	private Vector3 _newPos;
+    [SerializeField]
+    private float MinXPos;
+    [SerializeField]
+    private float MaxXPos;
+    [SerializeField]
+    private float MinYPos;
+    [SerializeField]
+    private float MaxYPos;
+
+
+
+    private Vector3 _newPos;
 	private Vector3 _oldPos;
 	void Start()
 	{
@@ -20,26 +31,26 @@ public class CameraMovement : MonoBehaviour {
 
 	void Update () 
 	{
-
-		if(Input.mousePosition.x >_theScreenWidth - _boundary)
+        Vector3 pos = transform.position;
+		if(Input.mousePosition.x >_theScreenWidth - _boundary && pos.x < MaxXPos)
 			{
 			_newPos.x += _speed * Time.deltaTime;
 			}
 
-		if(Input.mousePosition.x < 0 + _boundary)
+		if(Input.mousePosition.x < 0 + _boundary && pos.x > MinXPos)
 		{
 			_newPos.x -= _speed * Time.deltaTime;
 		}
 
-		if(Input.mousePosition.y > _theScreenHeight - _boundary)
+		if(Input.mousePosition.y > _theScreenHeight - _boundary && pos.y < MaxYPos)
 		{
 			_newPos.y += _speed * Time.deltaTime;
 		}
 
-		if(Input.mousePosition.y < 0 + _boundary)
+		if(Input.mousePosition.y < 0 + _boundary && pos.y > MinYPos)
 		{
 			_newPos.y -= _speed * Time.deltaTime;
 		}
-		transform.position = _newPos;
+		transform.position = Vector3.Lerp(transform.position, _newPos,0.1f);
 	}
 }

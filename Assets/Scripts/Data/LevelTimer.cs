754b66cec6c1549ab446ddb12f9ac6aea10c587e
Jprummel <jordiprummel@live.nl>
2016-05-20 12:22:38 +0200

    Small update again and level

--- Assets/Scripts/Data/LevelTimer.cs
+++ Assets/Scripts/Data/LevelTimer.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelTimer : MonoBehaviour {
     [SerializeField]private int         _startTimer;
     [SerializeField]private float       _timer;
     [SerializeField]private Text        _timerText;
+    [SerializeField]private Text        _levelCounter;
                     private LevelEnd    _levelEnd;
 
 	// Use this for initialization
 	void Start () {
         _levelEnd = GetComponent<LevelEnd>();
         _timer    = _startTimer;
+        _levelCounter.text = "Level : " + SceneManager.GetActiveScene().buildIndex.ToString();
 	}
 	
 	// Update is called once per frame

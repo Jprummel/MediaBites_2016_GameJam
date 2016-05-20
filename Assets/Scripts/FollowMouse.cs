8f781986871acaafeaea374a18161e451265f147
nathan-san <18964@ma-web.nl>
2016-05-20 07:16:02 +0200

    added mouse particles

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+    private Vector2 mousePos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = mousePos;
+    }
+}
